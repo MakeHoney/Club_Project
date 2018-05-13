b37aea38aa6ca6a444231f521d4a86a23d661c9d
MakeHoney <makehoney3@gmail.com>
2018-05-14 02:39:03 +0900

    modify method in club controller

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -92,23 +92,23 @@ class ClubsController < ApplicationController
 # 모집중이면서 메인 사진이 등록된 동아리의 메인 사진을 디비로부터 배열에 담아 반환해주는 메소드
 
 def imageAdvertise
-  @buffer = Array.new();
-  @buffer2 = Array.new();
+  ids = Array.new();
+  urls = Array.new();
   
-  sqlQuery = 'SELECT id FROM Clubs WHERE isApply = 1 AND isApply = 2';
+  sqlQuery = 'SELECT id FROM Clubs WHERE isApply = 1 OR isApply = 2';
   db = Club.connection;
-  @tuples = db.exec_query(sqlQuery);
-  @tuples.each do |tuple|
-    @buffer << tuple['id'];
+  tuples = db.exec_query(sqlQuery);
+  tuples.each do |tuple|
+    ids << tuple['id'];
   end
   
-  @buffer.each do |applying_id|
-    sqlQuery = "SELECT url FROM Photos WHERE club_id = #{elem} AND isMain = 0";
+  ids.each do |applying_id|
+    sqlQuery = "SELECT url FROM Photos WHERE club_id = #{applying_id} AND isMain = 0";
     db = Photo.connection;
-    @tuples = db.exec_query(sqlQuery);
-    @tuples.each do |tuple|
-      @buffer2 << tuple['url'];
+    tuples = db.exec_query(sqlQuery);
+    tuples.each do |tuple|
+      urls << tuple['url'];
     end
-    return @buffer2;
   end
+  return urls;
 end
